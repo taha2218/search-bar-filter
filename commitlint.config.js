8466d2dd6822b1b521e2c054054755616accfd62
Taha Shahid <45139576+taha2218@users.noreply.github.com>
2024-01-07 05:27:03 +0530

    Issue 5: Setup Husky And Commitlint
    * #5: Setup Husky And Commitlint
    
    * #5: Remove node_modules and package-lock.json from tracking node_modules and package-lock.json from tracking

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [2, 'always', ['#']],
+      'subject-full-stop': [2, 'never'],
+      'subject-max-length': [2, 'always', 50], // Change 50 to your desired max length
+      // Add other rules as needed
+    },
+    messages: {
+      'type-enum': 'Your commit message type must start with "#"',
+      'subject-full-stop': 'Remove the trailing full stop from the subject line',
+      'subject-max-length': 'Subject line should not exceed 50 characters', // Customize max length message
+      // Customize other messages as needed
+    },
+  };
+  
